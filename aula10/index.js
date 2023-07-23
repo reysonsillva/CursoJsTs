e8de75cd007c292530950146c3f29e124f2f95cb
Reyson Silva <reysonsillva@gmail.com>
2023-07-22 21:40:25 -0300

    operadores-aritimeticos

---
+++ aula10/index.js
@@ -0,0 +1,20 @@
+/* 
+Aritméticos :
++ Adição / Concatenação
+- Subtração
+* Multiplicação
+** Potenciação
+% Resto da divisão
+Ordem dos operadores: (), **, * / %, + - 
+Incremento = ++
+Decremento = --
+Operadores de atribuição: +=, -= *=, **=, %=, /=
+NaN = Not a number / parseInt (inteiro), parseFloat(decimais), 
+*/
+
+let contador = 1;
+contador ++;  // 2
+contador ++;  // 3
+contador ++;  // 4
+contador ++;  // 5
+console.log(contador)
