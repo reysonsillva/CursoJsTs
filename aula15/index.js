94f9a77f31f17d3c4e0cbeb4a72d0103f5e80d3c
Reyson Silva <reysonsillva@gmail.com>
2023-07-31 22:15:19 -0300

    numbers

---
+++ aula15/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.8116;
+// let num2 = Math.floor(num1); // Arredonda pra baixo
+// let num2 = Math.ceil(num1);  // Arredonda pra cima 
+// let num2 = Math.round(num1); // Arredonda pro mais próximo
+// console.log(Math.max(1, 2, 3, 4, 5, 6, 7, -10, -85, -64, 844, 284)); // Mair número
+// console.log(Math.min(1, 2, 3, 4, 5, 6, 7, -10, -85, -64, 844, 284)); // Menor número
+// const aleatorio = Math.round(Math.random() * (10-5) + 5);
+
+console.log(aleatorio);
