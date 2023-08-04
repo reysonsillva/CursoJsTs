c9b846fae45dd7de3b389a74eb802637fec007bd
Reyson Silva <reysonsillva@gmail.com>
2023-08-03 22:36:16 -0300

    arrays

---
+++ aula16/index.js
@@ -0,0 +1,16 @@
+//         012345678910
+
+const nome = 'Luiz Otávio';
+
+//                0        1       2
+const alunos = ['Luiz', 'Maria', 'João'];
+
+alunos.push('Denise')
+alunos.push('Reyson')
+
+// console.log(alunos.slice(0, -1));
+
+// const removido = alunos.shift()        // Remove do começo
+// const removido = alunos.pop()          // Remove do final
+// alunos.push('Reyson')                  // Add no começo
+// alunos.unshift('Denise')               // Add no final
