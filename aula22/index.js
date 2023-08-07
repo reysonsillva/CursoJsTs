3f282f5df921810a8a14caf77635ea716c950d87
Reyson Silva <reysonsillva@gmail.com>
2023-08-07 20:53:50 -0300

    aula22

---
+++ aula22/index.js
@@ -0,0 +1,11 @@
+/*
+    Operadores Lógicos:
+
+    &&  = AND  = E -> Todas as expressões precisam ser verdadeiras para retornar TRUE
+    ||  = OR   = Ou
+    !   = NOT  = Não
+*/
+
+const expressaoAnd = true && true && true && true
+const expressaOr =  
+console.log(expressaoAnd);
